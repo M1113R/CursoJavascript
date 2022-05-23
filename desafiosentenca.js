c40bd74a718f63a5f13932e37005e8b961559496
M1113r <g234930@dac.unicamp.br>
2022-05-23 13:27:05 -0300

    commit

---
+++ desafiosentenca.js
@@ -0,0 +1,17 @@
+{
+    for (let x = 1; x <= 9; x++) {
+        console.log(x);
+
+        x += 1;
+    }
+    console.log('Bloco 1 Finalizado');
+}
+
+{
+    for (let x = 2; x <= 10; x++) {
+        console.log(x);
+
+        x += 1;
+    }
+    console.log('Bloco 2 Finalizado');
+}
