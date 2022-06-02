7f46db2d821770bcfc887f3ddab9b6181fc08e2a
M1113r <g234930@dac.unicamp.br>
2022-06-02 16:43:14 -0300

    commit

---
+++ fundamentos/destructuring3.js
@@ -0,0 +1,11 @@
+function rand({ min = 0, max = 1000}) {
+    const valor = Math.random() * (max - min) + min;
+    return Math.floor(valor);
+}
+
+console.log(rand({ min: 0, max: 99999}));
+console.log(rand({ min: 0, max: 99999}));
+
+const obj = { max: 50, min: 40};
+console.log(rand(obj));
+
